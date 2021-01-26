09dd6ce688d8985062e0cd84e30358d417109035
Joel Almqvist <x_joeal@sigma.nsc.liu.se>
2021-01-26 15:04:30 +0100

    updated test

--- src/skepu3/cluster/gpi/cluster_test.cpp
+++ src/skepu3/cluster/gpi/cluster_test.cpp
@@ -7,38 +7,51 @@
 #include <reduce.hpp>
 #include <map.hpp>
 #include <omp.h>
+#include <ctime>
 
+int main(int argc, char *argv[]){
 
-int main(){
+  
 
-  auto square = skepu::Map<1>([](long a) long {
+  auto start = std::chrono::system_clock::now();
+  
+
+  auto square = skepu::Map<1>([](long a)  {
     return a * a;
   });
 
 
-  auto add = skepu::Map<2>([](long a, long b) long {
+  auto add = skepu::Map<2>([](long a, long b)  {
     return a + b;
   });
 
 
-  auto mult = skepu::Map<2>([](long a, long b) long {
+  auto mult = skepu::Map<2>([](long a, long b)  {
     return a * b;
   });
 
+  int i;
+   i = atoi(argv[1]);
+  //i = 32;
+
   // Constructor: Matrix(M, N initial value)
   // The Matrix interface will need to be tweaked to match SkePU's
-  skepu::Matrix<long> m1{1000, 100, 1};
-  skepu::Matrix<long> m2{1000, 100, 2};
-  skepu::Matrix<long> m3{1000, 100, 3};
+  skepu::Matrix<long> m1{10000, 1000, 1};
+  skepu::Matrix<long> m2{10000, 1000, 1};
+  skepu::Matrix<long> m3{10000, 1000, 1};
 
-  for(int i = 0; i < 10; i ++){
+  for(; i > 0; i --){
   add(m1, m2, m3);
   add(m2, m1, m3);
   add(m3, m2, m1);
 }
 
-  int res = m3.get(0);
-  std::cout << "Res = " << res << std::endl;
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end-start;
+
+  //int res = m3.get(0);
+  int res = 0;
+  std::cout << "Res = " << res << " calc time = " << elapsed_seconds.count() << std::endl;
 
 
   return 0;
